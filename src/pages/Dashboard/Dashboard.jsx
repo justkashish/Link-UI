8668248062b2cd51353ebc07a8669f7ec3eb3781
justkashish <meghanikashish1@gmail.com>
2025-02-01 01:26:20 +0530

    dashboard css

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useCallback } from "react"
 import { Bar, BarChart, ResponsiveContainer, Tooltip, YAxis } from "recharts"
-import "./dashboard.css"
+import "./Dashboard.css"
 
 export default function Dashboard() {
   const uri = import.meta.env.VITE_BACKEND_URL
